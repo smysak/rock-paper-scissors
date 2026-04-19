d4a5f2d513ed98604e4f4bf9388ab22fee58c6ba
smysak <98991422+smysak@users.noreply.github.com>
2026-04-19 15:53:50 +0800

    fix indentation to improve neatness of code

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function getComputerChoice() {
-    let computerChoice = Math.random();
-    console.log(computerChoice);
+	let computerChoice = Math.random();
+	console.log(computerChoice);
 
-    if (computerChoice <= 0.333) {
-        computerChoice = `rock`
-    } else if (computerChoice > 0.666) {
-        computerChoice = `scissors`
-    } else computerChoice = `paper`;
-    
-    console.log(computerChoice);
-    return computerChoice;
+	if (computerChoice <= 0.333) {
+		computerChoice = `rock`
+	} else if (computerChoice > 0.666) {
+		computerChoice = `scissors`
+	} else computerChoice = `paper`;
+
+	console.log(computerChoice);
+	return computerChoice;
 }
 
 
 function getHumanChoice() {
-    let humanChoice = prompt(`Make your choice:` , `rock, paper, or scissors`);
-    console.log(humanChoice);
-    return humanChoice;
+	let humanChoice = prompt(`Make your choice:` , `rock, paper, or scissors`);
+	console.log(humanChoice);
+	return humanChoice;
 }
 
 
@@ -25,35 +25,35 @@ let compyScore = 0;
 
 
 function playRound(humanChoice, computerChoice) {
-if (humanChoice === computerChoice) {
-    alert("Tie!");
-} else if (humanChoice === `scissors` && computerChoice === `paper`) {
-    alert("Scissors cut paper - You win!");
-    return ++humanScore;
-} else if (humanChoice === `rock` && computerChoice === `scissors`) {
-    alert("Rock breaks scissors - You win!");
-    return ++humanScore;
-} else if (humanChoice === `paper` && computerChoice === `rock`) {
-    alert("Paper covers rock - You win!");
-    return ++humanScore;
-} else if (humanChoice === `paper` && computerChoice === `scissors`) {
-    alert("Scissors cut paper - You lose!");
-    return ++compyScore;
-} else if (humanChoice === `scissors` && computerChoice === `rock`) {
-    alert("Rock breaks scissors - You lose!");
-    return ++compyScore;
-} else if (humanChoice === `rock` && computerChoice === `paper`) {
-    alert("Paper covers rock - You lose!");
-    return ++compyScore;
-} else alert("You're terrible at this game!");
+	if (humanChoice === computerChoice) {
+		alert("Tie!");
+	} else if (humanChoice === `scissors` && computerChoice === `paper`) {
+		alert("Scissors cut paper - You win!");
+		return ++humanScore;
+	} else if (humanChoice === `rock` && computerChoice === `scissors`) {
+		alert("Rock breaks scissors - You win!");
+		return ++humanScore;
+	} else if (humanChoice === `paper` && computerChoice === `rock`) {
+		alert("Paper covers rock - You win!");
+		return ++humanScore;
+	} else if (humanChoice === `paper` && computerChoice === `scissors`) {
+		alert("Scissors cut paper - You lose!");
+		return ++compyScore;
+	} else if (humanChoice === `scissors` && computerChoice === `rock`) {
+		alert("Rock breaks scissors - You lose!");
+		return ++compyScore;
+	} else if (humanChoice === `rock` && computerChoice === `paper`) {
+		alert("Paper covers rock - You lose!");
+		return ++compyScore;
+	} else alert("You're terrible at this game!");
 }
 
 function playGame() {
-    for (i=0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-        alert("You: " + humanScore + " Compy: " + compyScore);
-    }
-    alert("Final Score: You " + humanScore + " Compy: " + compyScore);
+	for (i=0; i < 5; i++) {
+		playRound(getHumanChoice(), getComputerChoice());
+		alert("You: " + humanScore + " Compy: " + compyScore);
+	}
+	alert("Final Score: You " + humanScore + " Compy: " + compyScore);
 }
 
 playGame();
